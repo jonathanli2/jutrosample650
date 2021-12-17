564ede415327d4a7a90e5ed8206b502a46bbedbe
Jonathan Li <jonathan_li2000@yahoo.ca>
2021-12-17 11:51:24 -0500

    add claim list component

--- src/pages/AgentOverview/AgentOverview.js
+++ src/pages/AgentOverview/AgentOverview.js
@@ -1,7 +1,10 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { TranslatorContext } from '@jutro/locale';
-
+import { MetadataForm } from '@jutro/uiconfig';
+import uiMetadata from './AgentOverview.metadata.json5';
+import { ClaimList } from './ClaimList/src/index';
+import listData from './ClaimList/src/claimListData.json';
 import styles from './AgentOverview.module.scss';
 import messages from './AgentOverview.messages';
 
@@ -9,9 +12,21 @@ export const AgentOverview = ({ title }) => {
     const translator = useContext(TranslatorContext);
     return (
         <div className={styles.agentOverview} title={title}>
-            {translator(messages.label)}
+          <MetadataForm
+            uiProps={uiMetadata['a.b.agentOverview']}
+            showOptional
+            classNameMap={styles}
+            componentMap = {{
+                ClaimList
+            }}
+            overrideProps= {{
+                claimList:  {
+                  formData: { claims: listData.claimList }
+                }
+            }}
+          />
         </div>
-    );
+      );
 };
 
 AgentOverview.propTypes = {
