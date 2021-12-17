8585b7e2b6c24e17b741812373e388caaee44c6b
Jonathan Li <jonathan_li2000@yahoo.ca>
2021-12-17 13:55:07 -0500

    enable filter of claim list

--- src/pages/AgentOverview/ClaimList/src/ClaimList.js
+++ src/pages/AgentOverview/ClaimList/src/ClaimList.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 import { MetadataForm } from '@jutro/uiconfig';
 import { Card } from '@business-patterns/components';
 import { IconColumn, StatusColumn, WarningColumn } from './columns';
 import uiMetadata from './ClaimList.metadata.json5';
+import cx from 'classnames';
+import { Link } from '@jutro/components';
+import { Flex, FlexItem } from '@jutro/layout';
+import { ActionTitleBar, TitleElement } from './TitleBars';
+import styles from './ClaimList.module.scss';
 
 const componentMap = {
     IconColumn: IconColumn,
@@ -16,7 +21,50 @@ const onSeeDetailsClick = row => {
 
 const onNewClaimClick = () => window.alert('New claim started');
 
+const filterButtonsConfig = [
+    { label: 'Open', filter: ({ state }) => state.code === 'open' },
+    { label: 'Processing', filter: ({ state }) => state.code === 'processing' },
+    { label: 'Approved', filter: ({ state }) => state.code === 'approved' },
+    { label: 'Declined', filter: ({ state }) => state.code === 'declined' },
+];
+
 export const ClaimList = ({ formData }) => {
+
+    const [activeFilter, setActiveFilter] = useState(filterButtonsConfig[0]);
+
+    const filteredData = useMemo(() => {
+        const claimList = formData.claims?.filter(activeFilter.filter);
+        return { claimList };
+      }, [activeFilter, formData]);
+
+      const filterButtons = useMemo(
+        () => (
+          <Flex
+            gap="small"
+            alignItems="center"
+            className={styles.filterButtonWrapper}
+          >
+            {filterButtonsConfig.map((config) => (
+              <FlexItem
+                grow="1"
+                key={config.label}
+                className={styles.filterButtonItem}
+              >
+                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                <Link
+                  onClick={() => setActiveFilter(config)}
+                  className={cx({
+                    [styles.activeFilterButton]: config.label === activeFilter.label,
+                  })}
+                >
+                  {config.label}
+                </Link>
+              </FlexItem>
+            ))}
+          </Flex>
+        ),
+        [activeFilter]
+      );
 
     const callbackMap = {
         onSeeDetailsClick,
@@ -25,9 +73,16 @@ export const ClaimList = ({ formData }) => {
 
     return (
         <Card maxWidth={1200}>
+            <ActionTitleBar>
+                <TitleElement>
+                    <h2>Claims List</h2>
+                </TitleElement>
+                {filterButtons}
+            </ActionTitleBar>
+
             <MetadataForm
                 uiProps={uiMetadata['claimList.view']}
-                data={{ claimList: formData.claims}}
+                data={filteredData}
                 callbackMap={callbackMap}
                 componentMap={componentMap}
             />
